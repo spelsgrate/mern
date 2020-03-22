72803dc389307a41b4ad0f6ffbe9a176058a0422
spelsgrate <antemratin@gmail.com>
2020-03-22 10:25:00 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+require('./server/routes/person.routes')(app); // This is new
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+})
